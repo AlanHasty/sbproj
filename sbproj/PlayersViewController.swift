86159a1cc253ac4de34b9990f52caa9736e88727
Alan Hasty <alan.hasty@gmail.com>
2015-10-08 07:30:51 -0500

    Custom table view design achieved

--- sbproj/PlayersViewController.swift
+++ sbproj/PlayersViewController.swift
@@ -39,6 +39,11 @@ class PlayersViewController: UITableViewController {
         return players.count
     }
 
+    func imageForRating(rating:Int) -> UIImage? {
+        let imageName = "\(rating)Stars"
+        return UIImage(named: imageName)
+    }
+    
     /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
@@ -49,16 +54,33 @@ class PlayersViewController: UITableViewController {
     }
     */
 
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath)
-        -> UITableViewCell {
-            let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath)
-            
-            let player = players[indexPath.row] as Player
-            cell.textLabel?.text = player.name
-            cell.detailTextLabel?.text = player.game
-            return cell
+//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath)
+//        -> UITableViewCell {
+//            let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath)
+//            
+//            let player = players[indexPath.row] as Player
+//            cell.textLabel?.text = player.name
+//            cell.detailTextLabel?.text = player.game
+//            return cell
+//    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath) //1
+        
+        let player = players[indexPath.row] as Player //2
+        
+        if let nameLabel = cell.viewWithTag(100) as? UILabel { //3
+            nameLabel.text = player.name
+        }
+        if let gameLabel = cell.viewWithTag(101) as? UILabel {
+            gameLabel.text = player.game
+        }
+        if let ratingImageView = cell.viewWithTag(102) as? UIImageView {
+            ratingImageView.image = self.imageForRating(player.rating)
+        }
+        return cell
     }
-
+    
     /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
