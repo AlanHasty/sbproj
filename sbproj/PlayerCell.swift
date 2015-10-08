d96eaebb9990cf1f30faa1179553e99203def420
Alan Hasty <alan.hasty@gmail.com>
2015-10-08 07:48:48 -0500

    Player Cell custom class.

---
+++ sbproj/PlayerCell.swift
@@ -0,0 +1,30 @@
+//
+//  PlayerCell.swift
+//  sbproj
+//
+//  Created by Alan Hasty on 10/8/15.
+//  Copyright © 2015 Alan Hasty. All rights reserved.
+//
+
+import UIKit
+
+class PlayerCell: UITableViewCell {
+
+    @IBOutlet weak var gameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    var player: Player! {
+        didSet {
+            gameLabel.text = player.game
+            nameLabel.text = player.name
+            ratingImageView.image = imageForRating(player.rating)
+        }
+    }
+    
+    func imageForRating(rating:Int) -> UIImage? {
+        let imageName = "\(rating)Stars"
+        return UIImage(named: imageName)
+    }
+
+}
